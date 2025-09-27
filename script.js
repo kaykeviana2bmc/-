81d6fe65cad19c61cdada65e6d79ab7800b88533
kaykeviana2bmc <kaykeviana2b@gmail.com>
2025-09-27 19:26:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const botoes = document.querySelectorAll('.produto button');
+
+botoes.forEach(botao => {
+  botao.addEventListener('click', () => {
+    alert('Produto adicionado ao carrinho!');
+  });
+});
