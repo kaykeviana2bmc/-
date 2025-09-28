49f17689d86de6c926988e17d712a8d675af8d68
kaykeviana2bmc <kaykeviana2b@gmail.com>
2025-09-27 21:48:55 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const botoes = document.querySelectorAll('.produto button');
-
-botoes.forEach(botao => {
-  botao.addEventListener('click', () => {
-    alert('Produto adicionado ao carrinho!');
-  });
-});
